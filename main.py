2275496048e756235770a07a839deeaa76da1251
Raju Mondal <mailmeraju92@gmail.com>
2026-02-22 20:53:59 +0530

    updated

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
@@ -34,3 +34,26 @@ async def get_filtered_students():
     return await collection.find({"marks": {"$gte": 60}}, {"_id": 0}).to_list(100)
 
 # new api here
+@app.get("/students/{name}")
+async def get_student_by_name(name:str):
+      studentName = await collection.find({"name": name},{"_id":0}).to_list(100)
+      if studentName:
+          return studentName
+      raise HTTPException(status_code=404, detail="Student not found, Name is incorrect or the student does not exist")
+
+@app.put("/students/{name}")
+async def update_student_marks(name:str,marks:int):
+    updateMarks = await collection.update_one({"name": name}, {"$set": {"marks": marks}})
+    if updateMarks.modified_count > 0:        return {"status": "200", "message": "Student marks updated successfully"}
+    # return {"status": "404", "message": "Student not found or marks already updated"}
+    raise HTTPException(status_code=404, detail="Student not found or marks already updated")
+
+    
+@app.delete("/students/{name}")
+async def delete_by_name(name:str):
+    delete_stdnt = await collection.delete_one({"name":name})
+    if delete_stdnt.deleted_count>0:
+        return {"status": "200", "message": "Student deleted successfully"}
+    # return {"status": "404", "message": "Student not found"}
+    raise HTTPException(status_code=404, detail="Student not found")
+
